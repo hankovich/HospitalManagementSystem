a34e2cc211f39341ebe073dc722a8f525c4f8fba
hankovich <svyatoslav.hankovich@epam.com>
2018-04-01 23:30:09 +0300

    style changes

--- HospitalManagementSystem.Client/Hms.UI/ViewModels/MainViewModel.cs
+++ HospitalManagementSystem.Client/Hms.UI/ViewModels/MainViewModel.cs
@@ -1,6 +1,17 @@
 ﻿namespace Hms.UI.ViewModels
 {
+    using System;
+    using System.Windows.Threading;
+
     public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            timer.Tick += (sender, args) => this.OnPropertyChanged(nameof(Time));
+            timer.Start();
+        }
+
+        public string Time => DateTime.Now.ToString("HH:mm:ss");
     }
 }
